c443e76b0eb0cb7110338080f8b9558b7686bb17
Thaha Ismail <TJMIsmail@in.imshealth.com>
2020-08-09 19:53:24 +0530

    First Commit on Ocelot Sample

---
+++ catalogAPI/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace catalogAPI.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet , Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "inr", "Jpy" };
+        }
+
+        
+    }
+}
---
+++ orderAPI/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace orderAPI.Controllers
+{
+    [Route("order-api/orders")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "order1", "order2" };
+        }
+
+        
+        // POST api/values
+
+    }
+}
